7e825f773ce916406059660cd747b4a0850e7087
TheOnly1TY <anifowosetemitayo5@gmail.com>
2025-04-28 11:30:30 -0700

    refactor: remove unused event listeners for quiz navigation

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -8,28 +8,6 @@ export function ProtectedRoute({ children }) {
   useEffect(
     function () {
       if (selectedQuestion.length === 0) navigate("/");
-      const handleBeforeUnload = (event) => {
-        event.preventDefault();
-        event.returnValue = "";
-      };
-      const handlePopState = () => {
-        const confirmLeave = window.confirm(
-          "Are you sure you want to end the quiz?"
-        );
-        if (confirmLeave) {
-          navigate("", { replace: true });
-        } else {
-          history.pushState(null, null, location.href);
-        }
-      };
-      history.pushState(null, null, location.href);
-      window.addEventListener("beforeunload", handleBeforeUnload);
-      window.addEventListener("popstate", handlePopState);
-
-      return () => {
-        window.removeEventListener("beforeunload", handleBeforeUnload);
-        window.removeEventListener("popstate", handlePopState);
-      };
     },
     [selectedQuestion, navigate]
   );
